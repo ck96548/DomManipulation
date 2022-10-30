11e8e000c654b7086113c9a81055db943f65d1e8
ck96548 <ck96548@gmail.com>
2022-10-30 23:16:59 +0530

    filter in Event Listener

--- event.js
+++ event.js
@@ -1,5 +1,6 @@
 let form = document.getElementById('addForm');
 let itemList = document.getElementById('items');
+let filter = document.getElementById('filter');
 
 form.addEventListener('submit',additem);
 
@@ -7,11 +8,12 @@ function additem(e){
     e.preventDefault();
 
     let newItem = document.getElementById('item').value;
+    let newItem1 = document.getElementById('item1').value;
     let li = document.createElement('li');
     
     li.className = 'list-group-item';
 
-    li.appendChild(document.createTextNode(newItem));
+    li.appendChild(document.createTextNode(newItem + " " + newItem1));
 
     itemList.append(li);
 
@@ -36,4 +38,24 @@ function removeItem(e){
         }
       
     }
+}
+
+filter.addEventListener('keyup',filterItem);
+
+function filterItem(e){
+
+    let text = e.target.value.toLowerCase();
+    let items = document.getElementsByTagName('li');
+    
+    Array.from(items).forEach(function(item){
+        let itemName = item.firstChild.textContent;
+
+        if(itemName.toLowerCase().indexOf(text) != -1){
+            item.style.display = 'block';
+        }else{
+            item.style.display = 'none';
+        }
+    })
+    
+
 }
